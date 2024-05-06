f1cf3c5d10686e12e504a833713e38a6bfd50e8a
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-06 15:08:01 +0200

    FEAT - list no style

--- index.php
+++ index.php
@@ -41,3 +41,44 @@
     ];
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hotel</title>
+</head>
+<body>
+    <!-- ciclo l'array hotels per generare in pagina un elemento con i dati di ogni hotel -->
+    <?php
+    // ciclo che restituisce ogni array contenete gli hotel
+        foreach ($hotels as $hotel){
+            // ciclo che restituisce ogni dato dell'hotel
+            ?>
+            <!-- div che contiene l'elemento con i dettagli dell'hotel -->
+            <div>
+                <?php
+                foreach ($hotel as $key => $value){
+                    if($key == 'name'){                        
+                        echo "<h2> $value </h2>";
+                    } elseif ($key == 'description'){
+                        echo "<p> $value </p>";
+                    } elseif ($key == 'parking'){
+                        if($value){
+                            echo "<p>L'hotel è dotato di parcheggio</p>";
+                        } else{
+                            echo "<p>L'hotel non è dotato di parcheggio</p>";
+                        }
+                    } elseif ($key == 'vote'){
+                        echo "<p> Voto: $value </p>";
+                    } else {
+                        echo "<p> Distanza dal centro: $value km</p>";
+                    }
+                }
+                ?>
+            </div>
+            <?php
+        }
+     ?>
+</body>
+</html>
